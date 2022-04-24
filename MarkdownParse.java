2f3a1ad072522b5c453064388ca0e251cce797ef
KunWang0129 <103203141+KunWang0129@users.noreply.github.com>
2022-04-23 17:43:11 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,6 +9,13 @@ public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
+        // checks whether all characters for links are in the text file
+        if(!markdown.contains("[") 
+        || !markdown.contains("]") 
+        || !markdown.contains("(")
+        || !markdown.contains(")")) { 
+            return toReturn;
+        }
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
@@ -16,11 +23,15 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            //makes sure loop doesnot check previous indices
             if(closeParen < currentIndex){
                 break;
             }
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            //makes sure the hyperlink is next to the url, making it a valid link
+            if(closeBracket + 1 == openParen) { 
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+            currentIndex = closeParen+1;
         }
         return toReturn;
     }
