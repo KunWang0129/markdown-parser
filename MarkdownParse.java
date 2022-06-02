410bfe70b3e971af74cdd525674b7e338f8e0b59
KunWang0129 <103203141+KunWang0129@users.noreply.github.com>
2022-06-01 21:08:03 -0700

    change

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,64 +5,37 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-//nmsl
-
-//add a line
-
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // checks whether all characters for links are in the text file
-        if(!markdown.contains("[") 
-        || !markdown.contains("]") 
-        || !markdown.contains("(")
-        || !markdown.contains(")")) { 
-            return toReturn;
-        }
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-
             int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-
-            //checks number of backticks in the brackets
-            /*int index = openBracket;
-            int tickNum = 0;
-            while(index < markdown.length()){
-                if(markdown.indexOf('`',index) == -1){
-                    break;
-                }
-                else if(markdown.indexOf('`', index) > openBracket &&
-                markdown.indexOf("`",index) < openParen){
-                    tickNum++;
-                }
-                index = markdown.indexOf('`', index) + 1;
-            }*/
-
-            //makes sure loop doesnot check previous indices
-            if(closeParen < currentIndex){
+		   if (openBracket == -1) {
                 break;
             }
-            //makes sure the hyperlink is next to the url, making it a valid link
-            if(!(closeBracket + 1 == openParen)) {  
-
+            int closeBracket = markdown.indexOf("]", openBracket);
+            if (closeBracket == -1) {
+                break;
             }
-            //makes sure that this isn't an image reference
-            else if((markdown.contains("!") 
-            && (markdown.indexOf("!",currentIndex) == openBracket - 1))) {
-            }
-            else{
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-            }
-            currentIndex = closeParen+1;
-
+            int openParen = markdown.indexOf("(", closeBracket);
+		if(openParen == -1){
+			break;
+		}
+          
+            int closeParen = markdown.indexOf(")", openParen);
+	if(closeParen == -1){
+		break;
+    }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
         }
+
         return toReturn;
     }
+
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
---
+++ MarkdownParse0.java
@@ -0,0 +1,73 @@
+//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+//nmsl
+
+//add a line
+
+public class MarkdownParse0 {
+
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // checks whether all characters for links are in the text file
+        if(!markdown.contains("[") 
+        || !markdown.contains("]") 
+        || !markdown.contains("(")
+        || !markdown.contains(")")) { 
+            return toReturn;
+        }
+        // find the next [, then find the ], then find the (, then read link upto next )
+        int currentIndex = 0;
+        while(currentIndex < markdown.length()) {
+
+            int openBracket = markdown.indexOf("[", currentIndex);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            int closeParen = markdown.indexOf(")", openParen);
+
+            //checks number of backticks in the brackets
+            /*int index = openBracket;
+            int tickNum = 0;
+            while(index < markdown.length()){
+                if(markdown.indexOf('`',index) == -1){
+                    break;
+                }
+                else if(markdown.indexOf('`', index) > openBracket &&
+                markdown.indexOf("`",index) < openParen){
+                    tickNum++;
+                }
+                index = markdown.indexOf('`', index) + 1;
+            }*/
+
+            //makes sure loop doesnot check previous indices
+            if(closeParen < currentIndex){
+                break;
+            }
+            //makes sure the hyperlink is next to the url, making it a valid link
+            if(!(closeBracket + 1 == openParen)) {  
+
+            }
+            //makes sure that this isn't an image reference
+            else if((markdown.contains("!") 
+            && (markdown.indexOf("!",currentIndex) == openBracket - 1))) {
+            }
+            else{
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+            currentIndex = closeParen+1;
+
+        }
+        return toReturn;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+	    System.out.println(links);
+    }
+}
